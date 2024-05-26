2a71fa8467b6e1a245a07320527e0dcb29f75aaa
Jimmys <90797256+jimmysrafael@users.noreply.github.com>
2024-05-26 19:44:50 -0400

    Update serve.js

--- src/serve.js
+++ src/serve.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-//const todosRoutes = require("./todos.routes");
+const todosRoutes = require("./todos.routes");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(todosRoutes);
 
 const port = process.env.PORT || 3333;
 
